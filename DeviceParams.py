66481a379b09a4fd68c3cc6c09eda27d5d701054
Max <maxfooo@gmail.com>
2015-09-03 14:25:15 -0700

    Include file with device parameters for DotModel

---
+++ DeviceParams.py
@@ -0,0 +1,14 @@
+'''
+Created on Sep 3, 2015
+
+@author: Max Ruiz
+'''
+from Tkinter import *
+root = Tk()
+root.withdraw()
+rd = StringVar()
+rs = StringVar()
+vto = StringVar()
+kp = StringVar()
+mosfetParams = {'rd' : rd, 'rs': rs, 'vto' : vto, 'kp' : kp}
+
